5fb58349321fec790aa9ec89dc98e965f477bd77
Sk.Muskan Fathima <124171813+SMILEYMUSKAN@users.noreply.github.com>
2023-09-26 11:16:30 +0000

    Loading Functionality Added - UserComponent

--- src/UsersComponent.js
+++ src/UsersComponent.js
@@ -2,19 +2,29 @@ import { useEffect, useState } from "react";
 
 var UsersComponent = () => {
   var [users, setUsers] = useState([]);
+  var [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((res) => res.json())
       .then((user) => setUsers(user));
   }, []);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
   return (
     <div className="border border-slate-500 max-w-fit m-auto mt-4 p-10 rounded  px-32 bg-red-400 text-white text-lg italic shadow">
       <h1 className="text-white text-4xl italic underline mb-5 text-center">
         Users
       </h1>
 
-      {users ? (
+      {loading ? (
+        <h1>Loading......</h1>
+      ) : (
         users.map((user) => (
           <div key={user.id} className="m-3">
             <h1 className="text-2xl text-slate-300">
@@ -25,8 +35,6 @@ var UsersComponent = () => {
             </h4>
           </div>
         ))
-      ) : (
-        <h1>Loading......</h1>
       )}
     </div>
   );
